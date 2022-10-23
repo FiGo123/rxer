e22574794a56236e5b55c43c06bbf7e1ddd8af5c
Filip Golovic <filipgolovicc@gmail.com>
2022-10-23 15:35:42 +0200

    Distinct, skip operators

--- app/src/main/java/com/speed/up/my/phone/rxer/Operators.kt
+++ app/src/main/java/com/speed/up/my/phone/rxer/Operators.kt
@@ -1,16 +1,22 @@
 package com.speed.up.my.phone.rxer
 
 import android.util.Log
+import com.speed.up.my.phone.rxer.data.User
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
-
-
-
-
-    fun justOperator(){
-        val mList = mutableListOf(1,2,3,4,5,6,7,8,9,10,11,12)
+class RxFuns {
+    val mUserList = mutableListOf<User>(
+        User(1, "First", 22),
+        User(2, "Second", 22),
+        User(3, "Third", 22),
+        User(4, "Fourth", 22),
+    )
+    fun justOperator() {
+        val mList = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
         val observable = Observable.just(mList)
 
         val observer = object : Observer<List<Int>> {
@@ -31,16 +37,15 @@ import io.reactivex.rxjava3.disposables.Disposable
             }
 
 
-
         }
         observable.subscribe(observer)
     }
 
-    fun fromOperator(){
-        val arraysNum = arrayOf(1,2,3,4,5,6,7,8,9,10,11,12)
+    fun fromOperator() {
+        val arraysNum = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
         val observable = Observable.fromArray(arraysNum)
 
-        val observer = object : Observer<Array<Int>>{
+        val observer = object : Observer<Array<Int>> {
             override fun onSubscribe(d: Disposable) {
                 TODO("Not yet implemented")
             }
@@ -62,7 +67,30 @@ import io.reactivex.rxjava3.disposables.Disposable
         observable.subscribe(observer)
     }
 
-    fun rangeOperator():Observable<Int>{
-        return Observable.range(1,10)
+    fun rangeOperator(): Observable<Int> {
+        return Observable.range(1, 10)
     }
 
+    fun repeatOperator(): Observable<Int> {
+        return Observable.range(1, 10).repeat()
+    }
+
+    fun intervalOperator(): Observable<Long> {
+        return Observable.interval(1, TimeUnit.SECONDS)
+    }
+
+    fun timerOperator(): Observable<Long> {
+        return Observable.timer(4, TimeUnit.SECONDS)
+    }
+
+    fun createOperator(): Observable<Int> {
+        return Observable.create(ObservableOnSubscribe<Int> {
+
+        })
+    }
+
+    fun filterOperator(): Observable<User> {
+        return Observable.fromIterable(mUserList)
+    }
+
+}
---
+++ app/src/main/java/com/speed/up/my/phone/rxer/data/User.kt
@@ -0,0 +1,7 @@
+package com.speed.up.my.phone.rxer.data
+
+data class User(
+    val id: Long,
+    val name: String,
+    val age: Int
+)
